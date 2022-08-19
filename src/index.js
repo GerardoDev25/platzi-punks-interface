a7228a4ddf698db12a2c1debc3be26f7a9ad8819
Gerardo Miranda <gerardodev25@gmail.com>
2022-08-19 13:40:00 -0400

    create the interface and connect with the contract

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { HashRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HashRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Web3ReactProvider } from '@web3-react/core';
+
+import App from './App';
+
+import { getLibrary } from './config/web3';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <ChakraProvider>
-        <App />
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <App />
+        </Web3ReactProvider>
       </ChakraProvider>
     </HashRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,30 @@
+import { useWeb3React } from '@web3-react/core';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { useState } from 'react';
+import { usePlatziPunks } from '../../hooks/useTruncatedAddress/usePlatziPunks';
 const Home = () => {
+  const [maxSupplay, setMaxSupplay] = useState();
+
+  const platziPunks = usePlatziPunks();
+
+  const { active } = useWeb3React();
+
+  const getMaxSupply = useCallback(async () => {
+    if (platziPunks) {
+      const result = await platziPunks.methods.maxSupply().call();
+      setMaxSupplay(result);
+    }
+  }, [platziPunks]);
+
+  useEffect(() => {
+    getMaxSupply();
+  }, [getMaxSupply]);
+
+  if (!active) return 'Conecta tu wallet';
   return (
     <>
-      <p>Hello world </p>
+      <p>max Supply: {maxSupplay}</p>
     </>
   );
 };
